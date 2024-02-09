42115b39d96b91b30194d6a0d4e26bfe968b9d2b
Hemanga619 <hemangabharali619@gmail.com>
2024-02-09 23:35:10 +0530

    prototype21

--- Kriti2024/Assets/Scripts/DoorButton.cs
+++ Kriti2024/Assets/Scripts/DoorButton.cs
@@ -1,17 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorButton : MonoBehaviour
 {
     [SerializeField] private GameObject door;
 
-    private void OnCollisonEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             Destroy(door);
         }
     }
-
 }
