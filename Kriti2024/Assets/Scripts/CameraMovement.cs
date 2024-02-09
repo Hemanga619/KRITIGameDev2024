dc1ba5ead1d89dbd74c6087678f24289a71578fe
Hemanga619 <hemangabharali619@gmail.com>
2024-02-09 19:44:29 +0530

    prototype16

--- Kriti2024/Assets/Scripts/CameraMovement.cs
+++ Kriti2024/Assets/Scripts/CameraMovement.cs
@@ -1,40 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
     private Vector3 CameraPosition;
-    public float CameraSpeed = 0.01f;
-    // Start is called before the first frame update
+    private float CameraSpeed = 0.01f;
+    [SerializeField] private Vector3 centerPoint;
+    private Vector3 originalCamPos;
+    private float originalZoomView;
+    [SerializeField] private float zoomView;
+    private Camera MainCamera;
+    private bool isZoom;
+
     void Start()
     {
+        isZoom = false;
+        MainCamera = GetComponent<Camera>();
         CameraPosition = this.transform.position;
+        originalCamPos = transform.position;
+        originalZoomView = MainCamera.orthographicSize;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W)){
-            CameraPosition.y += CameraSpeed;
-        }
-        if(Input.GetKey(KeyCode.S)){
-            CameraPosition.y -= CameraSpeed;
-        }
-        if(Input.GetKey(KeyCode.D)){
-            CameraPosition.x += CameraSpeed;
-        }
-        if(Input.GetKey(KeyCode.A)){
-            CameraPosition.x -= CameraSpeed;
-        }
+        if (Input.GetKeyDown(KeyCode.Z))
+            isZoom = !(isZoom);
 
-        this.transform.position = CameraPosition;
+        if (isZoom)
+        {
+            transform.position = centerPoint;
+            MainCamera.orthographicSize = zoomView;
+        }
+        else
+        {
+            transform.position = new Vector3(originalCamPos.x, originalCamPos.y, originalCamPos.z);
+            MainCamera.orthographicSize = originalZoomView;
 
-    }
-    
+            if (Input.GetKey(KeyCode.W))
+            {
+                CameraPosition.y += CameraSpeed;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                CameraPosition.y -= CameraSpeed;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                CameraPosition.x += CameraSpeed;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                CameraPosition.x -= CameraSpeed;
+            }
 
-    private void FixedUpdate()
-    {
-        
+            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+            {
+                CameraPosition.y += CameraSpeed * 1.5f;
+            }
+            if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.LeftShift))
+            {
+                CameraPosition.y -= CameraSpeed * 1.5f;
+            }
+            if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift))
+            {
+                CameraPosition.x += CameraSpeed * 1.5f;
+            }
+            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.LeftShift))
+            {
+                CameraPosition.x -= CameraSpeed * 1.5f;
+            }
+
+            this.transform.position = CameraPosition;
+            originalCamPos = transform.position;
+        }
     }
 }
